6abc818227ee2b8b5c05e3d8ac0877d638020852
7J647 <007J647@gmail.com>
2020-10-08 19:58:19 -0400

    created the methods to execute the necessary MySQL commands in the controllers

--- config/orm.js
+++ config/orm.js
@@ -30,6 +30,80 @@ function printQuestionMarks(num) {
   }
   
 
+  // Object for all our SQL statement functions.
+  var orm = {
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+
+    insertOne: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    updateOne: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+  
+    // delete: function(table, col, value, cb) {
+    //   var queryString = "DELETE " + table;
+  
+    //   queryString += " DELETE FROM";
+    //   queryString += objToSql(objColVals);
+    //   queryString += " WHERE ";
+    //   queryString += condition;
+  
+    //   console.log(queryString);
+    //   DELETE FROM ?? WHERE ?? = ?, [table, col, value, ]
+    //   connection.query("DELETE FROM ?? WHERE ?? = ?", [table, col, value, ](function(err, result) {
+    //     if (err) {
+    //       throw err;
+    //     }
+  
+    //     cb(result);
+    //   });
+    // }
+  };
+
+
+
+
 // * Export the ORM object in `module.exports`.
 
 
